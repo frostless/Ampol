4e0c704aaf50d8f9f9d1575235400700624a757e
Jason Zheng <jason.zheng@vantagefx.com>
2021-10-16 18:05:08 +1100

    Implement service

--- src/Ampol-API/Services/CounterService.cs
+++ src/Ampol-API/Services/CounterService.cs
@@ -1,33 +1,90 @@
-﻿using Ampol_API.Models;
+﻿using System;
+using System.Linq;
+using Ampol_API.Data;
+using Ampol_API.Models;
 
 namespace Ampol_API.Services
 {
     public class CounterService : ICounterService
     {
+        private readonly DataContext _dataContext;
+
+        public CounterService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
         public Receipt Checkout(Purchase purchase)
         {
             decimal totalAmount = 0;
             decimal totalDiscounts = 0;
             int totalPoints = 0;
 
-           foreach (var basket in purchase.Baskets)
+            foreach (var basket in purchase.Baskets)
             {
                 totalAmount += basket.TotalPrice;
-                totalPoints += GetPromitionPoints(basket);
-                totalDiscounts += GetPromitionDiscount(basket);
+                totalPoints += GetPromotionPoints(basket, purchase.TransactionDate);
+                totalDiscounts += GetPromotionDiscount(basket, purchase.TransactionDate);
             }
 
             return new Receipt(purchase.CustomerId, purchase.LoyaltyCard, purchase.TransactionDate, totalAmount, totalDiscounts, totalPoints);
         }
 
-        public int GetPromitionPoints(Basket basket)
+        public int GetPromotionPoints(Basket basket, DateTime date)
         {
-            return 0;
+            var products = _dataContext.GetProducts();
+            var pointsPromotion = _dataContext.GetPointsPromotion();
+
+            var product = products.FirstOrDefault(p => p.Id == basket.ProductId);
+
+            if (product == default)
+            {
+                // Search miss, no points given
+                return 0;
+            }
+
+            var matchPromotion = pointsPromotion.Where(pp => pp.Category == product.Category)
+                                        .Where(pp => pp.StartDate >= date && pp.EndDate <= date)
+                                         // Only one oints promo can run so take the highest by default
+                                         .OrderByDescending(pp => pp.PointsPerDollar)
+                                        .SingleOrDefault();
+
+            if (matchPromotion == default)
+            {
+                // Search miss, no points given
+                return 0;
+            }
+
+            return matchPromotion.PointsPerDollar * (int)Math.Ceiling(basket.TotalPrice);
         }
 
-        public decimal GetPromitionDiscount(Basket basket)
+        public decimal GetPromotionDiscount(Basket basket, DateTime date)
         {
-            return 0;
+            var discountPromotion = _dataContext.GetDiscountPromotion();
+            var discountPromotionProducts = _dataContext.GetDiscountPromotionProducts();
+
+            var matchPromotionProducts = discountPromotionProducts.Where(dpp => dpp.ProductId == basket.ProductId).ToList();
+
+            if (matchPromotionProducts?.Count == 0)
+            {
+                // Search miss, no points given
+                return 0;
+            }
+
+            var matchPromotion = discountPromotion.Where(dp => matchPromotionProducts.Any(mpp => mpp.DiscountPromotionId == dp.Id))
+                                                  .Where(dp => dp.StartDate >= date && dp.EndDate <= date)
+                                                 // Only one oints promo can run so take the highest by default
+                                                  .OrderByDescending(dp => dp.Percentage)
+                                                  .SingleOrDefault();
+
+            if (matchPromotion == default)
+            {
+                // Search miss, no points given
+                return 0;
+            }
+
+
+            return matchPromotion.Percentage * basket.TotalPrice;
         }
     }
 }
--- src/Ampol-API/Services/ICounterService.cs
+++ src/Ampol-API/Services/ICounterService.cs
@@ -1,11 +1,12 @@
-﻿using Ampol_API.Models;
+﻿using System;
+using Ampol_API.Models;
 
 namespace Ampol_API.Services
 {
     public interface ICounterService
     {
         public Receipt Checkout(Purchase purchase);
-        public int GetPromitionPoints(Basket basket);
-        public decimal GetPromitionDiscount(Basket basket);
+        public int GetPromotionPoints(Basket basket, DateTime date);
+        public decimal GetPromotionDiscount(Basket basket, DateTime date);
     }
 }
